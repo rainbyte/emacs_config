8041e24595ca554a90c01f9248570a6ade198039
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2015-05-30 03:09:44 -0300

    Launch prog-mode from vala-mode-hook

--- init.el
+++ init.el
@@ -271,6 +271,7 @@
 (add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
 (add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))
 (defun my:vala-mode-config()
+  (run-hooks 'prog-mode-hook)
   (setq c-basic-offset 4)
   (c-set-style "java"))
 (add-hook 'vala-mode-hook 'my:vala-mode-config)
